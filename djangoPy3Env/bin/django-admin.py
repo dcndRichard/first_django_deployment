f6fcbdbf842cda4c5d81cc5f22f2637c5a93b11b
Duane Richard <duanerichard76@yahoo.com>
2019-12-12 18:41:33 -0800

    initial commit

---
+++ djangoPy3Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/duane/DigitalDuane/sandbox/CodingDojo/python_track/django/django_intro/assignments/first_django_project_assignment/djangoPy3Env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
